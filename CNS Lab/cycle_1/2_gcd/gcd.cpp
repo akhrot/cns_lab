78e2e3f1495bac34ac2e45b4162bec407a19eff5
Jinish Raiyarela <jinishraiyarela@gmail.com>
2018-08-29 14:49:23 +0530

    Add files via upload

---
+++ CNS Lab/cycle_1/2_gcd/gcd.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<string>
+
+#include<gmp.h>
+#include<assert.h>
+using namespace std;
+
+void gcd( mpz_t &a, mpz_t&b )
+{
+	if( mpz_cmp( a, b ) < 0 )
+		mpz_swap( a, b );
+	
+	mpz_t r;
+	mpz_init( r );
+	mpz_mod( r, a, b );
+	
+	while( mpz_cmp_ui( r, 0 )>0 )
+	{
+		mpz_set( a, b );
+		mpz_set( b, r );
+		
+		mpz_mod( r, a, b );
+	}
+	
+	mpz_set( a, b );
+}
+
+int main()
+{
+	string a_str, b_str;
+	mpz_t a, b;
+	
+	cout<<"Enter the numbers a and b : ";
+	cin>>a_str>>b_str;
+	
+	mpz_init( a );
+	mpz_init( b );
+	mpz_set_ui( a, 0 );
+	mpz_set_ui( b, 0 );
+	
+	assert( mpz_set_str( a, a_str.c_str(), 10 )==0 );
+	assert( mpz_set_str( b, b_str.c_str(), 10 )==0 );
+	
+	cout<<"a = "<<a<<endl;
+	cout<<"b = "<<b<<endl;
+	
+	clock_t start_time = clock();
+	
+	gcd( a, b );
+	
+	clock_t end_time = clock();
+	
+	cout<<"\ngcd(a, b) = "<<a<<" Time taken = "<<(double)(end_time-start_time)/(CLOCKS_PER_SEC)*1000<<"ms\n";
+	
+	mpz_clear(a);
+	mpz_clear(b);
+	
+	return 0;
+}
---
+++ CNS Lab/gmp_basics/first.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+#include<gmp.h>
+#include<assert.h>
+using namespace std;
+
+int main()
+{
+	char input[100005];
+	
+	mpz_t n;
+	int flag;
+	
+	cout<<"Enter a number: ";
+	cin>>input;
+	
+	mpz_init( n );
+	mpz_set_ui( n, 0 );
+	
+	flag = mpz_set_str( n, input, 10 );
+	assert( flag==0 );
+	cout<<"n = "<<n<<endl;
+	
+	mpz_add_ui( n, n, 1 );
+	cout<<"n+1 = "<<n<<endl;
+	
+	mpz_mul( n, n, n );
+	cout<<"(n+1)^2 = "<<n<<endl;
+	
+	mpz_clear(n);
+	
+	return 0;
+}
