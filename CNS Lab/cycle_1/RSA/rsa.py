4eeaf8360dfec9634647a2af23ccc50bb274602d
akhrot <jinishraiyarela@gmail.com>
2018-10-31 13:41:05 +0530

    RSA implementation in Python

--- CNS Lab/cycle_1/RSA/rsa.py
+++ CNS Lab/cycle_1/RSA/rsa.py
@@ -1,7 +1,54 @@
-def main():
-	p, q = list(map(int,input( "Enter the prime numbers p and q" ).split()))
-	print p, q
+import math
 
-	print type(p), type(q) 
+def gcdExt( a, m ):
+	a = int(a%m)
+	b = int(m)
+
+	xold = 1
+	x = 0
+	xnew = -1
+	while( b>1 ):
+		q = a//b
+		r = a%b
+
+		xnew = xold - q*x
+		xold = x
+		x = xnew
+
+		a = b
+		b = r
+
+	x = x%m
+
+	return x
+
+def find_coprime_e( totient, start = 2 ):
+	for e in range(start, totient-1):
+		if math.gcd(e, totient)==1:
+			return e
+	return -1
+
+def main():
+	p, q = map(int, ( input( "Enter the 2 prime numbers : " ).split() ) );
+
+	n = p*q
+	totient = (p-1)*(q-1)
+	print("n = p*q =", n)
+	print("totient(n) =", totient)
+
+	e = find_coprime_e( totient )
+	print("e =", e)
+
+	d = gcdExt(e, totient)
+	print("d =", d)
+
+	msg = int( input("Enter the number to be encrypted between " + str(1) + " and " + str(totient-1) + " : ") )
+
+	cipherText = pow( msg, e, n )
+	decipheredText = pow(cipherText, d, n)
+
+	print("Ciphertext :", cipherText)
+	print( "Deciphered Text :", decipheredText )
+
 
 main()
